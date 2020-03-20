e738888a37912b24cfeb3df50b094d7c7678669d
Gabrielle Ynara <gabrielleynara@ymail.com>
2020-03-20 17:05:19 -0500

    Node MongoDB Example 2

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const MongoClient = require('mongodb').MongoClient; // It will act as a client for the mongo server
 const assert = require('assert').strict;
+const dboper = require('./operations'); // dboper = short for database operations
 
 const url = 'mongodb://localhost:27017/';
 const dbname = 'nucampsite';
@@ -17,29 +18,41 @@ MongoClient.connect(url, { useUnifiedTopology: true }, (err, client) => {
      * Checks if err is equal to null. If so the application keeps running forward,
      * otherwise the assert will fail and terminate the whole application and log the error to the console. 
      */
-    assert.strictEqual(err, null); 
+    assert.strictEqual(err, null);
 
     console.log('Connected correctly to server');
 
     const db = client.db(dbname); //Connects to nucampsite data base on the MongoDb server
 
-    db.dropCollection('campsites', (err, result) => { // Deletes database table
-        assert.strictEqual(err, null);
-        console.log('Dropped Collection', result);
+    db.dropCollection('campsites', (err, result) => {
+        console.log('Dropped Collection: ', result);
 
-        const collection = db.collection('campsites');
+        dboper.insertDocument(db, { name: "Breadcrumb Trail Campground", description: "Test"},
+            'campsites', result => {
+            console.log('Insert Document:', result.ops); // ops is short for operations
 
-        collection.insertOne({ name: "BreadCrumb Trail Campground", description: "Test"}, 
-        (err, result) => {
-            assert.strictEqual(err, null);
-            console.log('Insert Document: ', result.ops); // ops is short for operations
-
-            collection.find().toArray((err, docs) => { //toArray convert the data so it's possible to console log it
-                assert.strictEqual(err, null);
-                console.log('Found Documents: ', docs);
-
-                client.close();
-            });  
-         });
-     }); 
+                dboper.findDocuments(db, 'campsites', docs => {
+                    console.log('Found Documents:', docs);
+    
+                    dboper.updateDocument(db, { name: "Breadcrumb Trail Campground" },
+                        { description: "Updated Test Description" }, 'campsites',
+                        result => {
+                            console.log('Updated Document Count:', result.result.nModified);
+    
+                            dboper.findDocuments(db, 'campsites', docs => {
+                                console.log('Found  Documents:', docs);
+                                
+                                dboper.removeDocument(db, { name: "Breadcrumb Trail Campground" },
+                                    'campsites', result => {
+                                    console.log('Deleted Document Count:', result.deletedCount);
+    
+                                    client.close();
+                                });
+                            });
+                        }
+                    );
+                });
+            }
+        );
+    });
 });
